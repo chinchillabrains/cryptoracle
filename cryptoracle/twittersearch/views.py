9c4c009bc121fe735cfbfefb6cfd4704e1cdc510
Ilias P <chinchillabrains@gmail.com>
2022-06-07 15:36:53 +0300

    Analyze sentiment with textblob

--- cryptoracle/twittersearch/views.py
+++ cryptoracle/twittersearch/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 from . import bridge
 from .search import search_tweets
 from .search import count_tweets
+from analyzesentiment import analyze
 
 import json
 
 # Create your views here.
 def home(request):
     # tweet_counts = bridge.get_tweet_counts('bitcoin')
-    tweet_counts = bridge.get_tweets('bitcoin')
-    ret_str = json.dumps(tweet_counts)
+    # tweet_counts = bridge.get_tweets('bitcoin')
+    # ret_str = json.dumps(tweet_counts)
     # ret_str = "\n".join(search_tweets('bitcoin'))
-    return HttpResponse(ret_str)
+    for tweet in search_tweets('bitcoin'):
+        print(tweet)
+        print(analyze.get_text_sentiment(tweet).subjectivity)
+    return HttpResponse('Done!')
