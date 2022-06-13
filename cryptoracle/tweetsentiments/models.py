43813d0bc2c04a624b8aba9e8c962417753cff98
Ilias P <chinchillabrains@gmail.com>
2022-06-13 12:17:36 +0300

    Add tweetsentiments model

--- cryptoracle/tweetsentiments/models.py
+++ cryptoracle/tweetsentiments/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from cryptoprices.models import Crypto
 
+class Sentiments(models.Model):
+    coin = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+    date = models.DateField()
+    keywords = models.CharField(max_length=100)
+    sentiment = models.DecimalField(max_digits=1, decimal_places=6)
+    search_volume = models.IntegerField()
+    polarization = models.DecimalField(max_digits=1, decimal_places=6)
