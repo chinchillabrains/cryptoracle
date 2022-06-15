2abb4158222885cfa5d9a36f6f77cb128d32cfb6
Ilias P <chinchillabrains@gmail.com>
2022-06-15 14:56:49 +0300

    Add start_time in twitter bridge

--- cryptoracle/twittersearch/bridge.py
+++ cryptoracle/twittersearch/bridge.py
@@ -2,11 +2,12 @@
 import os
 import requests
 
-def get_tweets(search_term, max_results='10', next_token=None):
+def get_tweets(search_term, max_results='10', next_token=None, start_time=None):
     next_page_query = '&next_token=' + next_token if next_token is not None else ''
+    start_time_query = '&start_time=' + start_time if start_time is not None else ''
     # query = 'https://api.twitter.com/2/tweets/search/recent?query={} lang:en -is:retweet&max_results=10'.format(search_term)
     # Do we need requests.utils.quote to format request before sending?
-    query = 'https://api.twitter.com/2/tweets/search/recent?query={} lang:en -is:retweet&max_results={}{}'.format(requests.utils.quote(search_term), max_results, next_page_query)
+    query = 'https://api.twitter.com/2/tweets/search/recent?query={} lang:en -is:retweet&max_results={}{}{}'.format(requests.utils.quote(search_term), max_results, next_page_query, start_time_query)
     return twitter_request(query)
 
 # Returns tweet counts for search_term in the last 7 days in 1-hour intervals
