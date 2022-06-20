00cfd258c161cd88063ec6142e98bcd92eb821c9
Ilias P <chinchillabrains@gmail.com>
2022-06-20 21:41:49 +0300

    Added crypto foreign key to tweets db

--- cryptoracle/twittersearch/models.py
+++ cryptoracle/twittersearch/models.py
@@ -1,9 +1,14 @@
+from ast import keyword
 from django.db import models
 from .customfields import BatchedValuesField
+from cryptoprices.models import Crypto
 
 # date
 # tweets
+# keyword
 # Temporarily save batches of tweets to get sentiment later & delete
 class Tweets(models.Model):
     date = models.DateField()
+    keyword = models.ForeignKey(Crypto, on_delete=models.CASCADE)
     tweets = BatchedValuesField(token='-splitweets-') # Something unique as token to split tweets
+
