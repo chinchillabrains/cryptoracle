84c0f31ab7f796719e60b0f3e258a971972dd67a
Ilias P <chinchillabrains@gmail.com>
2022-06-21 00:10:19 +0300

    gathered tweet counts

--- cryptoracle/twittersearch/get_tweets_cron.py
+++ cryptoracle/twittersearch/get_tweets_cron.py
@@ -16,3 +16,10 @@ def gather_tweets(keyword):
         if len(tweets) > 0:
             # Save to db
             Tweets.objects.create(date = yesterday, tweets = tweets)
+
+def gather_tweet_counts():
+    yesterday = (datetime.datetime.today() - datetime.timedelta(1)).strftime('%Y-%m-%d')
+    cryptos = Crypto.objects.all()
+    for crypto in cryptos:
+        count = get_yesterdays_tweet_counts(crypto.id)
+        Tweetcounts.objects.create(date=yesterday, crypto=crypto, count=count)
