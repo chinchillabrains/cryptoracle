d6d250287754fd70e3ab06cf6b5da68afd6ab75b
Ilias P <chinchillabrains@gmail.com>
2022-06-22 19:08:36 +0300

    build query - twitter bridge object-oriented

--- cryptoracle/twittersearch/bridgeoo.py
+++ cryptoracle/twittersearch/bridgeoo.py
@@ -13,9 +13,14 @@ class Bridge:
     def get_tweet_counts(self):
         pass
 
-    def build_query(self, service='search-recent', term=''):
+    def build_query(self, service='search-recent', term='', params={}):
+        api_base = 'https://api.twitter.com/2/'
         service_base = self.get_service_url(service)
-        query = 'https://api.twitter.com/2/'
+        keyword_query = '?query="{}" lang:en -is:retweet'.format(term)
+        params_str = '&'.join('{}={}'.format(key, params[key]) for key in params.keys())
+        full_query = '{}{}{}{}'.format(api_base, service_base, keyword_query, params_str)
+        return full_query
+
 
     def api_get(self, query):
         bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
