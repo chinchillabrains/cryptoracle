7cc6a2ee6f4ae71d3e22df11b1a3a3b9a75e9cd7
Ilias P <chinchillabrains@gmail.com>
2022-06-22 18:56:14 +0300

    Add only tweet search & tweet count in O-O bridge

--- cryptoracle/twittersearch/bridgeoo.py
+++ cryptoracle/twittersearch/bridgeoo.py
@@ -14,7 +14,7 @@ class Bridge:
         pass
 
     def build_query(self, service='search-recent', term=''):
-        service_base, service_after = self.get_service_url(service)
+        service_base = self.get_service_url(service)
         query = 'https://api.twitter.com/2/'
 
     def api_get(self, query):
@@ -24,15 +24,15 @@ class Bridge:
 
     def get_service_url(self, service):
         service_base = ''
-        service_after = ''
+        # service_after = ''
         if service == 'search-recent':
             service_base = 'tweets/search/recent'
         elif service == 'counts-recent':
             service_base = 'tweets/counts/recent'
-        elif service == 'user':
-            service_base = 'users/by/username/'
-        elif service == 'user-tweets':
-            service_base = 'tweets/counts/recent'
-            service_after = '/tweets/'
+        # elif service == 'user':
+        #     service_base = 'users/by/username/'
+        # elif service == 'user-tweets':
+        #     service_base = 'tweets/counts/recent'
+        #     service_after = '/tweets/'
 
-        return (service_base, service_after)
+        return service_base
