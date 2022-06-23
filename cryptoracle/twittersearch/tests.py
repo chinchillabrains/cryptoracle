86d90254ca73f819b84eaed641488b0cd29adc2a
Ilias P <chinchillabrains@gmail.com>
2022-06-23 15:28:24 +0300

    Create test for oo bridge.test_service_url

--- cryptoracle/twittersearch/tests.py
+++ cryptoracle/twittersearch/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .bridgeoo import Bridge
 
-# Create your tests here.
+class TwitterBridgeTests(TestCase):
+    # def startUp():
+    #     pass
+
+    def test_service_url(self):
+        inputOutput = {
+            'search-recent': 'tweets/search/recent',
+            'counts-recent': 'tweets/counts/recent',
+        }
+        
+        bridge = Bridge()
+        for inVar in inputOutput.keys():
+            outVar = inputOutput[ inVar ]
+            searchQuery = bridge.get_service_url(inVar)
+            self.assertEquals(searchQuery, outVar)
+
+
+
+    # def test_build_query():
+    #     pass
+    
+    # def get_tweets():
+    #     pass
+
+    # def get_tweet_counts():
+    #     pass
