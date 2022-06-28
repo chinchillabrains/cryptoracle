8a5b864b7618f9fdfc065fcaf9c9c2f184e00eb3
Ilias P <chinchillabrains@gmail.com>
2022-06-28 10:28:57 +0300

    tweetsentiments admin - Show all table fields

--- cryptoracle/tweetsentiments/admin.py
+++ cryptoracle/tweetsentiments/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Sentiments
 
-admin.site.register(Sentiments)
+class SentimentsSettingsAllFields(admin.ModelAdmin):
+    list_display = [field.name for field in Sentiments._meta.get_fields()]
+
+admin.site.register(Sentiments, SentimentsSettingsAllFields)
