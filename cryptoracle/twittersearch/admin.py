e49ac2f28a6ae156a1e3ab3f1cf25081f723fd9a
Ilias P <chinchillabrains@gmail.com>
2022-06-28 10:29:32 +0300

    twittersearch admin - Show all table fields

--- cryptoracle/twittersearch/admin.py
+++ cryptoracle/twittersearch/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Tweets, Tweetcounts
 
-admin.site.register(Tweets)
-admin.site.register(Tweetcounts)
+class TweetsSettingsAllFields(admin.ModelAdmin):
+    list_display = [field.name for field in Tweets._meta.get_fields()]
+
+class CountsSettingsAllFields(admin.ModelAdmin):
+    list_display = [field.name for field in Tweetcounts._meta.get_fields()]
+
+admin.site.register(Tweets, TweetsSettingsAllFields)
+admin.site.register(Tweetcounts, CountsSettingsAllFields)
