f0cca2d992ef0c61d80ed61f0a1e02b4e378eb8b
Ilias P <chinchillabrains@gmail.com>
2022-06-28 10:28:18 +0300

    cryptoprices admin - Show all table fields

--- cryptoracle/cryptoprices/admin.py
+++ cryptoracle/cryptoprices/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Crypto
 from .models import Prices
 
-# Register your models here.
-admin.site.register(Crypto)
-admin.site.register(Prices)
+class CryptoSettingsAllFields(admin.ModelAdmin):
+    list_display = [field.name for field in Crypto._meta.get_fields() if field.related_model is None]
+
+class PricesSettingsAllFields(admin.ModelAdmin):
+    list_display = [field.name for field in Prices._meta.get_fields()]
+
+admin.site.register(Crypto, CryptoSettingsAllFields)
+admin.site.register(Prices, PricesSettingsAllFields)
