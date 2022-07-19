23719c1c609384764f3e5e351e03b476049bf56b
Ilias P <chinchillabrains@gmail.com>
2022-07-19 16:12:35 +0300

    Add celery broker url

--- cryptoracle/cryptoracle/settings.py
+++ cryptoracle/cryptoracle/settings.py
@@ -126,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
